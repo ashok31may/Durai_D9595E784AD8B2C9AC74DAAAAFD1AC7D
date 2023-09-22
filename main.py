186612db9c58fb7b8ecb28829e7b8655868c0be3
DuraiDass <144983049+DuraiDass@users.noreply.github.com>
2023-09-22 14:10:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-# Python 3 program to find 
-# factorial of given number
-  
-# Function to find factorial of given number
-def factorial(n):
-       
-    if n == 0:
-        return 1
-      
-    return n * factorial(n-1)
-   
-# Driver Code
-num = 5;
-print("Factorial of", num, "is",
-factorial(num))
-   
-# This code is contributed by Smitha Dinesh Semwal
+class Bank_Account:
+
+  def __init__(self):
+    self.balance = 0
+    print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+
+  def deposit(self):
+    amount = float(input("Enter amount to be Deposited: "))
+    self.balance += amount
+    print("\n Amount Deposited:", amount)
+
+  def withdraw(self):
+    amount = float(input("Enter amount to be Withdrawn: "))
+    if self.balance >= amount:
+      self.balance -= amount
+      print("\n You Withdrew:", amount)
+    else:
+      print("\n Insufficient balance  ")
+
+  def display(self):
+    print("\n Net Available Balance=", self.balance)
+
+
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
